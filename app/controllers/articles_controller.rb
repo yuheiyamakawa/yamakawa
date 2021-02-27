186e381e50dcf7d634fab4a5a8c28fc92673360c
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-02-27 21:37:10 +0900

    記事一覧と詳細ページ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
 
     def index
-        @article = Article.first
+        @articles = Article.all
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+
 
 end
